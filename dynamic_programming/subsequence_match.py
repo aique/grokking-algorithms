4c20f2696e054ff55bccbce03b2689914a4fea4e
Aique <aiquefernandez@gmail.com>
2021-09-18 19:48:42 +0200

    chapter 9 - dynamic programming - avoid hash sort

--- dynamic_programming/subsequence_match.py
+++ dynamic_programming/subsequence_match.py
@@ -2,33 +2,56 @@
 # diccionario ordenadas por coincidencia con respecto a una palabra de referencia.
 # Utiliza para ello programación dinámica.
 
-def get_suggestions(dictionary, word, num_sugestions):
-    matches = {}
 
-    for dictionary_word in dictionary:
-        grid = [[0 for i in range(len(dictionary_word))] for j in range(len(word))]
-        row = 0
+def get_num_matches(candidate, word):
+    grid = [[0 for i in range(len(candidate))] for j in range(len(word))]
+    row = 0
 
-        for current_char in word:
-            col = 0
+    for word_char in word:
+        col = 0
 
-            for dictionary_char in dictionary_word:
-                if dictionary_char == current_char:
-                    grid[row][col] = grid[row - 1][col - 1] + 1
-                else:
-                    grid[row][col] = max(grid[row - 1][col], grid[row][col - 1])
+        for current_char in candidate:
+            if current_char == word_char:
+                grid[row][col] = grid[row - 1][col - 1] + 1
+            else:
+                grid[row][col] = max(grid[row - 1][col], grid[row][col - 1])
 
-                col = col + 1
+            col = col + 1
 
-            row = row + 1
+        row = row + 1
 
-        matches[dictionary_word] = grid[len(grid) - 1][len(grid[0]) - 1]
+    return grid[len(grid) - 1][len(grid[0]) - 1]
 
-    return {k: v for k, v in sorted(matches.items(), key=lambda item: item[1], reverse=True)[:num_sugestions]}
+
+def compose_suggestions_response(matches_by_word):
+    suggestions = []
+
+    for i in range(len(matches_by_word)):
+        suggestions.append(matches_by_word[i][0])
+
+    return suggestions
+
+
+def get_suggestions(dictionary, word, num_suggestions):
+    matches_by_word = [['', -1] for i in range(min(len(dictionary), num_suggestions))]
+
+    for candidate in dictionary:
+        matches = get_num_matches(candidate, word)
+
+        for i in range(len(matches_by_word)):
+            # si se encuentra un mejor resultado, se mueven a la derecha los anteriores y se inserta en esa posición
+            if matches > matches_by_word[i][1]:
+                for j in range(i, len(matches_by_word) - 1):
+                    matches_by_word[j + 1] = matches_by_word[j]
+
+                matches_by_word[i] = [candidate, matches]
+                break
+
+    return compose_suggestions_response(matches_by_word)
 
 
 dictionary = set(['shed', 'mail', 'solve', 'crouch', 'fate'])
-word = 'soteouch'
+word = 'shedmavefate'
 
 found = False
 
